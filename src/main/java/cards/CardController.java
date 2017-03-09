9869efe211b3d144e478cef9cc3e51b7733646d0
lpknight21 <CyberExodus3>
2017-03-08 22:18:00 -0600

    Updated name to be path variable.

--- src/main/java/cards/CardController.java
+++ src/main/java/cards/CardController.java
@@ -1,8 +1,8 @@
 package cards;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +17,7 @@ public class CardController {
     }
 
     @RequestMapping("/name/{name}")
-    public Card getCardByName(@RequestParam String name) {
+    public Card getCardByName(@PathVariable String name) {
         return cardRepository.getCardByName(name);
     }
 }
